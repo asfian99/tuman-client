1093a0a77a07bf27166fb0f4a5bacb33141e088d
Khoirul Asfian <mk.asfian@gmail.com>
2021-03-31 23:05:37 +0700

    add nprogress loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import NProgress from "nprogress";
+import Router, { useRouter } from "next/router";
 import Layout from "../components/Layout";
-import { useRouter } from "next/router";
+import HTMLHead from "../components/HTMLHead";
+
+Router.events.on("routeChangeStart", (url: string) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   const { pathname } = useRouter();
@@ -9,13 +18,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Tuman | Tugas Management App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+        <HTMLHead />
       </Head>
 
       {pathname === "/login" || pathname === "/register" ? (
